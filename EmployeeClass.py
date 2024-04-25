6e0e192271ce9cfabb729c08a40adac5adca5d4a
AbdulRauf-007 <114727367+AbdulRauf-007@users.noreply.github.com>
2024-04-26 04:00:47 +0700

    Create EmployeeClass.py

---
+++ EmployeeClass.py
@@ -0,0 +1,7 @@
+class Employee:
+    def __init__(self, name, employee_id):
+        self.name = name
+        self.employee_id = employee_id
+
+    def display_info(self):
+        print(f"Name: {self.name}\nEmployee ID: {self.employee_id}")
